5b61b297959a10a35542f8c7669975b0bb771422
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2022-08-16 17:19:02 -0300

    feat: create game loop

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use std::error::Error;
 use rusty_audio::Audio;
-
+use std::io;
+use std::time::Duration;
+use crossterm::{terminal, ExecutableCommand, event};
+use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
+use crossterm::cursor::{Hide, Show};
+use crossterm::event::{Event, KeyCode};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut audio = Audio::new();
@@ -13,7 +18,36 @@ fn main() -> Result<(), Box<dyn Error>> {
     audio.add("win", "src/resources/audio/win.wav");
 
     audio.play("startup");
+    //Terminal Initializing
+    let mut terminal_stdout = io::stdout();
+    terminal::enable_raw_mode()?; //? -> Crash if error
 
+    terminal_stdout.execute(EnterAlternateScreen)?;
+    terminal_stdout.execute(Hide)?;
+
+    //Game Loop
+
+    'gameloop: loop {
+        //Input
+        while event::poll(Duration::default())? {
+            if let Event::Key(key_event) = event::read()? {
+                match key_event.code {
+                    KeyCode::Esc | KeyCode::Char('q') => {
+                        audio.play("lose");
+                        break 'gameloop;
+                    }
+                    _ => {}
+                }
+            }
+        }
+    }
+
+
+    //Cleanup and await
     audio.wait();
+    terminal_stdout.execute(Show)?;
+    terminal_stdout.execute(LeaveAlternateScreen)?;
+
+    terminal::disable_raw_mode()?;
     Ok(())
 }
