ea9540e5d05a1016566d493c1f4a92aa3a85ff4d
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2022-08-16 17:08:27 -0300

    feat: add initial audio feature

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use rusty_audio::Audio;
+
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut audio = Audio::new();
+
+    audio.add("lose", "src/resources/audio/lose.wav");
+    audio.add("explode", "src/resources/audio/explode.wav");
+    audio.add("move", "src/resources/audio/move.wav");
+    audio.add("pew", "src/resources/audio/pew.wav");
+    audio.add("startup", "src/resources/audio/startup.wav");
+    audio.add("win", "src/resources/audio/win.wav");
+
+    audio.play("startup");
+
+    audio.wait();
+    Ok(())
 }
